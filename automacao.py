7df5e3c59cb5c95f55676065d0eb1697a0e8eee3
NicolasVMarques <nicolas.marques26@hotmail.com>
2025-11-10 11:16:30 -0300

    Adicionado novos valores dos eixos de cada variavel

--- automacao.py
+++ automacao.py
@@ -4,13 +4,15 @@ import pandas as pd
 
 tabela = pd.read_excel('Cópia de modelo para robo indicação.xlsx')
 
-campo_primeiro_nome_x, campo_primeiro_nome_y = 541, 506
-campo_sobrenome_x, campo_sobrenome_y = 1532, 563
-campo_email_x, campo_email_y = 518, 677
-campo_telefone_x, campo_telefone_y = 1179, 674
-campo_razao_social_x, campo_razao_social_y = 500, 753
-campo_cnpj_x, campo_cnpj_y = 1557, 741
-botao_confirmar_x, botao_confirmar_y = 600, 700
+campo_primeiro_nome_x, campo_primeiro_nome_y = 385, 356
+campo_sobrenome_x, campo_sobrenome_y = 396, 404
+campo_email_x, campo_email_y = 360, 490
+campo_telefone_x, campo_telefone_y = 1074, 489
+campo_razao_social_x, campo_razao_social_y = 544, 575
+campo_cnpj_x, campo_cnpj_y = 842, 575
+botao_confirmar_x, botao_confirmar_y = 683, 618
+botao_leads_x, botao_leads_y = 591, 175
+botao_novo_lead_x, botao_novo_lead_y = 674, 236
 email_padrao = 'empresario@gmail.com'
 
 print("Iniciando automação em 5 segundos...")
@@ -25,7 +27,6 @@ for index, row in tabela.iterrows():
     telefone = str(row['Telefone'])
     cnpj = str(row['CNPJ'])
     email = str(row['EMAIL'])
-
 
     if pd.isna(email) or email.strip() == '':
         email_final = email_padrao
--- posicao.py
+++ posicao.py
@@ -1,7 +1,7 @@
 import time
 import pyautogui
 
-time.sleep(10)
+time.sleep(5)
 print(pyautogui.position())
 
 pyautogui.scroll(200)
